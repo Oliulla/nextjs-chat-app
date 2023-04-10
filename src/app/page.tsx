a9aff969ae82ab8b5fc9a8b7f1ba6a26e81280e4
oliullah <razasm29@gmail.com>
2023-04-11 02:05:04 +0600

    user auth and redisdb --> 1:07:00hrs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  await db.set("hello", "hello");
 
   return (
-    <div className="text-red-500">
-      <h1>hello world</h1>
-    </div>
+    <Button>Hello</Button>
   );
 }
